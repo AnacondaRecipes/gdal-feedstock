535f288f46b679caac701c7ce3c500f4a49a9b7f
Michael Sarahan <msarahan@gmail.com>
2018-04-20 09:59:50 -0500

    combine with libgdal in split output recipe; update 2.2.3

---
+++ recipe/install_lib.bat
@@ -0,0 +1,23 @@
+nmake /f makefile.vc devinstall %BLD_OPTS%
+if errorlevel 1 exit 1
+
+copy *.lib %LIBRARY_LIB%\ || exit 1
+if errorlevel 1 exit 1
+
+set ACTIVATE_DIR=%PREFIX%\etc\conda\activate.d
+set DEACTIVATE_DIR=%PREFIX%\etc\conda\deactivate.d
+mkdir %ACTIVATE_DIR%
+mkdir %DEACTIVATE_DIR%
+
+copy %RECIPE_DIR%\scripts\activate.bat %ACTIVATE_DIR%\gdal-activate.bat
+if errorlevel 1 exit 1
+
+copy %RECIPE_DIR%\scripts\deactivate.bat %DEACTIVATE_DIR%\gdal-deactivate.bat
+if errorlevel 1 exit 1
+
+:: Copy unix shell activation scripts, needed by Windows Bash users
+copy %RECIPE_DIR%\scripts\activate.sh %ACTIVATE_DIR%\gdal-activate.sh
+if errorlevel 1 exit 1
+
+copy %RECIPE_DIR%\scripts\deactivate.sh %DEACTIVATE_DIR%\gdal-deactivate.sh
+if errorlevel 1 exit 1
