56906e383b53b939c6283668099beddee8489471
Jonathan Helmus <jjhelmus@gmail.com>
2018-12-12 16:12:56 -0600

    indent configure argument to match conda-forge

--- recipe/build.sh
+++ recipe/build.sh
@@ -22,35 +22,35 @@ fi
 # See https://github.com/conda-forge/gdal-feedstock/pull/47 for the discussion.
 
 bash configure --prefix=$PREFIX \
-            --host=$HOST \
-            --with-curl \
-            --with-dods-root=$PREFIX \
-            --with-expat=$PREFIX \
-            --with-freexl=$PREFIX \
-            --with-geos=$PREFIX/bin/geos-config \
-            --with-hdf4=$PREFIX \
-            --with-hdf5=$PREFIX \
-            --with-jpeg=$PREFIX \
-            --with-kea=$PREFIX/bin/kea-config \
-            --with-libiconv-prefix=$PREFIX \
-            --with-libjson-c=$PREFIX \
-            --with-libkml=$PREFIX \
-            --with-liblzma=yes \
-            --with-libtiff=$PREFIX \
-            --with-libz=$PREFIX \
-            --with-netcdf=$PREFIX \
-            --with-openjpeg=$PREFIX \
-            --with-pcre \
-            --with-pg=$PREFIX/bin/pg_config \
-            --with-png=$PREFIX \
-            --with-poppler=$PREFIX \
-            --with-spatialite=$PREFIX \
-            --with-sqlite3=$PREFIX \
-            --with-proj4=$PREFIX \
-            --with-xerces=$PREFIX \
-            --with-xml2=$PREFIX \
-            --without-python \
-            --verbose \
-            ${OPTS}
+               --host=$HOST \
+               --with-curl \
+               --with-dods-root=$PREFIX \
+               --with-expat=$PREFIX \
+               --with-freexl=$PREFIX \
+               --with-geos=$PREFIX/bin/geos-config \
+               --with-hdf4=$PREFIX \
+               --with-hdf5=$PREFIX \
+               --with-jpeg=$PREFIX \
+               --with-kea=$PREFIX/bin/kea-config \
+               --with-libiconv-prefix=$PREFIX \
+               --with-libjson-c=$PREFIX \
+               --with-libkml=$PREFIX \
+               --with-liblzma=yes \
+               --with-libtiff=$PREFIX \
+               --with-libz=$PREFIX \
+               --with-netcdf=$PREFIX \
+               --with-openjpeg=$PREFIX \
+               --with-pcre \
+               --with-pg=$PREFIX/bin/pg_config \
+               --with-png=$PREFIX \
+               --with-poppler=$PREFIX \
+               --with-spatialite=$PREFIX \
+               --with-sqlite3=$PREFIX \
+               --with-proj4=$PREFIX \
+               --with-xerces=$PREFIX \
+               --with-xml2=$PREFIX \
+               --without-python \
+               --verbose \
+               ${OPTS}
 
 make -j ${CPU_COUNT} ${VERBOSE_AT}
