2734f6f500b9d97c45838343e6ff896f10a51ace
Kai Tietz <ktietz@anaconda.com>
2021-08-20 18:01:25 +0200

    add osx-arm64 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,8 @@
 
 set -e # Abort on error.
 
+cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* .
+
 # recommended in https://gitter.im/conda-forge/conda-forge.github.io?at=5c40da7f95e17b45256960ce
 find ${PREFIX}/lib -name '*.la' -delete
 
